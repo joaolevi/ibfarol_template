d20fa0fc4de32c7c27a585b08daffdf8ab1c3d69
joaolevi <joaolevi777@gmail.com>
2024-01-28 08:23:03 -0300

    Removing Firebase Setup

--- src/utils/firebaseSetup.js
+++ src/utils/firebaseSetup.js
@@ -4,14 +4,6 @@ import { getAuth } from "firebase/auth";
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://support.google.com/firebase/answer/7015592
-const firebaseConfig = {
-  apiKey: "AIzaSyAhie7j6jnDm_LNl1eCrSXQMUm6Kx-Kj1I",
-  authDomain: "ibfarol-c9cf2.firebaseapp.com",
-  projectId: "ibfarol-c9cf2",
-  storageBucket: "ibfarol-c9cf2.appspot.com",
-  messagingSenderId: "807186360476",
-  appId: "1:807186360476:web:c1709da7960b74ed762559"
-};
 
 // const firebaseConfig = {
 //   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
